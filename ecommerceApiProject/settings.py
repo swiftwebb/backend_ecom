dfde10c242deb2510957fa69499a9f91aec94b3b
Quadri <idogunkuadry@gmail.com>
2025-09-24 09:00:25 +0100

    yo

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -27,9 +27,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")     # ["*"] 
-# CSRF_TRUSTED_ORIGINS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")     # ["*"] 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 # Application definition
 
 INSTALLED_APPS = [
